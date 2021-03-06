d6225f26bda95e96a401bd7820386e1614c5489a
tangesana <tangesana1016@gmail.com>
2021-03-06 22:36:15 +0900

    管理画面に管理者設定を作成

---
+++ src/application/admin/controllers/Register.php
@@ -0,0 +1,23 @@
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Register extends CI_Controller
+{
+    public function __construct()
+    {
+        //CI_Model constructorの呼び出し
+        parent::__construct();
+        $this->load->model('register_model');
+        date_default_timezone_set('Asia/Tokyo');
+    }
+
+    public function index()
+    {    
+        $this->load->view('register_view');
+    }
+
+
+}
+
+
+?>
--- src/application/admin/views/register_view.php
+++ src/application/admin/views/register_view.php
@@ -1,187 +1,220 @@
 <!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
 <html lang="ja">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>会員情報</title>
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="<?= base_url('css/admin.css'); ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ぷちはぐ管理者画面</title>
+
+    <!-- for responsive -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    <!-- ionicons -->
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- adminLTE style -->
+    <link href="../../dist/css/adminlte.min.css" rel="stylesheet" type="text/css" />
+
+
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+    <div class="wrapper">
+        <!-- 上部ナビゲーション -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="index3.html" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto btn-li">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <button onclick="location.href='/admin.php/administrator/logout'" type="button" class="btn btn-block btn-info">ログアウト</button>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+            <!-- SEARCH FORM -->
+            <form class="form-inline ml-3">
+                <div class="input-group input-group-sm">
+                    <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-navbar" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?= base_url('dist/img/top_icon.svg'); ?>" alt="User Image">
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+
+                <!-- Notifications Dropdown Menu -->
+                <li class="nav-item">
+                    <div class="dropdown-menu-right">
+                        <form action="/logout" method="POST">
+                            <button type="submit" class="btn btn-primary btn-sm">ログアウト</button>
+                        </form>
+                    </div>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- トップメニュー -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- ロゴ -->
+            <a href="admin_starter.html" class="brand-link">
+                <img src="../../dist\img\logo1.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">管理画面</span>
+            </a>
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    機能一覧
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="admin_contents.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>コンテンツ管理</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="admin_user.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>利用者管理</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>DM管理</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item menu-open">
+                            <a href="#" class="nav-link active">
+                                <i class="nav-icon fas fa-cog"></i>
+                            <p>
+                                管理者設定
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                <a href="<?= base_url('admin.php/register/index'); ?>" class="nav-link active">
+                                    <i class="nav-icon far far fa-check-square"></i>
+                                    <p>新規登録</p>
+                                </a>
+                                </li>
+                                <li class="nav-item">
+                                <a href="<?= base_url('admin.php/caretaker/index'); ?>" class="nav-link">
+                                    <i class="nav-icon far far fa-check-square"></i>
+                                    <p>登録情報一覧</p>
+                                </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>管理者新規登録</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">ホーム</a></li>
+                                <li class="breadcrumb-item active">管理者新規登録</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- Main content -->
+            <section class="content">
+                <div class="register-box">
+
+                    <div class="card">
+                        <div class="card-body register-card-body">
+                            <!-- <?= form_open('register/index'); ?>
+                            <p class="register-error"><?= $register_error; ?></p> -->
+                                <div class="input-group mb-3">
+                                <input type="text" name="user_id" class="form-control" placeholder="Id">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                <input type="password" name="pass" class="form-control" placeholder="Password" autocomplete="off">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                <input type="password" name="pass_conf" class="form-control" placeholder="Retype password" autocomplete="off">
+                                <div class="input-group-append">
+                <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+                </div>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-8">
+            <?= validation_errors(); ?>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <button type="submit" name="register_submit" value='register' class="btn btn-primary btn-block">登録</button>
+            </div>
+            <!-- /.col -->
+            </div>
+        </form>
         </div>
-        <div class="info">
-          <a href="<?= base_url('admin.php/administrator/index'); ?>" class="d-block">管理画面</a>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-           <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon far fa-user"></i>
-                <p>
-                  会員設定
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="<?= base_url('admin.php/user/index'); ?>" class="nav-link">
-                    <i class="nav-icon far far fa-check-square"></i>
-                    <p>会員情報一覧</p>
-                  </a>
-                </li>
-              </ul>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="<?= base_url('admin.php/board/index'); ?>" class="nav-link">
-                    <i class="nav-icon far far fa-check-square"></i>
-                    <p>投稿一覧</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-cog"></i>
-                <p>
-                  管理者設定
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="<?= base_url('admin.php/register/index'); ?>" class="nav-link">
-                    <i class="nav-icon far far fa-check-square"></i>
-                    <p>新規登録</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="<?= base_url('admin.php/caretaker/index'); ?>" class="nav-link">
-                    <i class="nav-icon far far fa-check-square"></i>
-                    <p>登録情報一覧</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+        <!-- /.form-box -->
+    </div><!-- /.card -->
     </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1></h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active"></li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
- <!-- Main content -->
- <section class="content">
-<div class="register-box">
-        <div class="register-logo">
-            <b>管理者新規登録</b>
-        </div>
-
-  <div class="card">
-    <div class="card-body register-card-body">
-    <?= form_open('register/index'); ?>
-    <p class="register-error"><?= $register_error; ?></p>
-        <div class="input-group mb-3">
-          <input type="text" name="user_id" class="form-control" placeholder="Id">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" name="pass" class="form-control" placeholder="Password" autocomplete="off">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" name="pass_conf" class="form-control" placeholder="Retype password" autocomplete="off">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-          <?= validation_errors(); ?>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" name="register_submit" value='register' class="btn btn-primary btn-block">登録</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
-</div>
-<!-- /.register-box -->
-    </section>
-    <!-- /.content -->
+    <!-- /.register-box -->
+        </section>
+        <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
@@ -190,7 +223,7 @@
     <div class="float-right d-none d-sm-inline">
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2020 自転車コンセルジュ.</strong> All rights reserved.
+    <strong>Copyright &copy; 2021 幼児向け学習サイト　ぷちはぐ</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
