1c14b7cada20427950296c060018030cc6085fb7
tangesana <tangesana1016@gmail.com>
2021-03-07 12:45:42 +0900

    register_view.phpの作成

--- src/application/admin/views/register_view.php
+++ src/application/admin/views/register_view.php
@@ -168,92 +168,93 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Main content -->
             <section class="content">
                 <div class="register-box">
-
                     <div class="card">
                         <div class="card-body register-card-body">
-                            <!-- <?= form_open('register/index'); ?>
-                            <p class="register-error"><?= $register_error; ?></p> -->
-                                <div class="input-group mb-3">
-                                <input type="text" name="user_id" class="form-control" placeholder="Id">
-                                <div class="input-group-append">
-                                    <div class="input-group-text">
-                                    <span class="fas fa-user"></span>
+                            <?= form_open('register/index'); ?>
+                                    <p class="register-error">
+                                        <!-- <?= $register_error; ?> -->
+                                    </p>
+                                    <div class="input-group mb-3">
+                                        <input type="email" name="email" class="form-control" placeholder="email">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="fas fa-envelope"></span>
+                                                </div>
+                                            </div>
                                     </div>
-                                </div>
-                                </div>
-                                <div class="input-group mb-3">
-                                <input type="password" name="pass" class="form-control" placeholder="Password" autocomplete="off">
-                                <div class="input-group-append">
-                                    <div class="input-group-text">
-                                    <span class="fas fa-lock"></span>
+                                    <div class="input-group mb-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Password" autocomplete="off">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="fas fa-lock"></span>
+                                                </div>
+                                            </div>
                                     </div>
-                                </div>
-                                </div>
-                                <div class="input-group mb-3">
-                                <input type="password" name="pass_conf" class="form-control" placeholder="Retype password" autocomplete="off">
-                                <div class="input-group-append">
-                <div class="input-group-text">
-                <span class="fas fa-lock"></span>
+                                    <div class="input-group mb-3">
+                                        <input type="password" name="pass_conf" class="form-control" placeholder="Retype password" autocomplete="off">
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">
+                                                <span class="fas fa-lock"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-8">
+                                            <!-- <?= validation_errors(); ?> -->
+                                        </div>
+                                        <!-- /.col -->
+                                        <div class="col-4">
+                                            <button type="submit" name="register_submit" value='register' class="btn btn-primary btn-block">登録</button>
+                                        </div>
+                                        <!-- /.col -->
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.card -->
                 </div>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-8">
-            <?= validation_errors(); ?>
-            </div>
-            <!-- /.col -->
-            <div class="col-4">
-                <button type="submit" name="register_submit" value='register' class="btn btn-primary btn-block">登録</button>
-            </div>
-            <!-- /.col -->
-            </div>
-        </form>
+                <!-- /.register-box -->
+            </section>
+            <!-- /.content -->
         </div>
-        <!-- /.form-box -->
-    </div><!-- /.card -->
+        <!-- /.content-wrapper -->
+
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2021 幼児向け学習サイト　ぷちはぐ</strong> All rights reserved.
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
-    <!-- /.register-box -->
-        </section>
-        <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <!-- ./wrapper -->
 
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2021 幼児向け学習サイト　ぷちはぐ</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="../../plugins/jszip/jszip.min.js"></script>
+    <script src="../../plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="../../plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
 </body>
 </html>
