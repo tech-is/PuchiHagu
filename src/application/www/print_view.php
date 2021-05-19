44e9fc7a67edc352e38125e41af2d2cf24df3cdc
yamakubi mikio <mickey_o.0209@icloud.com>
2021-05-19 16:36:25 +0900

    #91 print.htmlのコードをprint_view.phpにコピーしました

---
+++ src/application/www/print_view.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>幼児無料プリントサイト ぷちはぐ</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="無料で利用できる幼児(3歳～6歳)向けの教育、プリントサービスです">
+  <meta name="keywords" content="幼児教育,知育,無料プリント,子供プリント,ひらがな,印刷">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="topdist/style.css">
+  <link rel="stylesheet" href="topdist/slide.css">
+  <script src="topdist/js/fixmenu_pagetop.js"></script>
+  <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+  <div id="container">
+
+    <header>
+      <h1 id="logo"><a href="Top/index"><img src="topdist/images/logo1.png" alt="幼児無料プリントサイト　ぷちはぐ"></a></h1>
+    </header>
+
+
+    <nav id="menubar">
+      <ul>
+        <li class="current"><a href="index.html">Home</a></li>
+        <li><a href="signup.html">会員登録・ログイン</a></li>
+        <li><a href="print.html">プリントする</a></li>
+        <li><a href="oshirase.html">お知らせ・コラム</a></li>
+        <li><a href="aboutsite.html">このサイトについて・利用規約</a></li>
+        <li><a href="contact.html">お問い合わせ</a></li>
+      </ul>
+    </nav>
+
+    <div id="contents">
+
+      <section class="box1">
+            <h2><span>プリント</span></h2>
+
+            <table cellpadding="0" cellspacing="10" align="center">
+              <tbody>
+                <tr>
+                  <td>
+                    <img src="topdist/images/hiragana.png" alt="ひらがなプリント" width="300" height="">
+                  </td>
+                  <td>
+                    <a href="hiragana.html" class="btn btn-malformation1">「ひらがな」を印刷</a>
+                  </td>
+                  <td>
+                    <img src="topdist/images/suzi.png" alt="すうじプリント" width="300" height=""></p>
+                  </td>
+                  <td>
+                    <a href="suzi.html" class="btn btn-malformation2">「すうじ」を印刷</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <table cellpadding="0" cellspacing="30" align="center">
+              <tbody>
+                <tr>
+                  <td>
+                    <img src="topdist/images/unpitsu.png" alt="ひらがなプリント" width="300" height="">
+                  </td>
+                  <td>
+                    <a href="unpitu.html" class="btn btn-malformation3">「運筆」を印刷</a>
+                  </td>
+                  <td>
+                    <img src="topdist/images/tentunagi.png" alt="すうじプリント" width="300" height=""></p>
+                  </td>
+                  <td>
+                    <a href="tentunagi.html" class="btn btn-malformation4">「点つなぎ」を印刷</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <table cellpadding="0" cellspacing="20" align="center">
+              <tbody>
+                <tr>
+                  <td>
+                    <img src="topdist/images/programming.png" alt="ひらがなプリント" width="300" height="">
+                  </td>
+                  <td>
+                    <a href="programming.html" class="btn btn-malformation5">「プログラミング」を印刷</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+      </section>
+
+    </div>
+    <!--/#contents-->
+
+    <footer>
+      <small>Copyright&copy; <a href="index.html">幼児無料プリントサイト　ぷちはぐ</a> All Rights Reserved.</small>
+      <span class="pr">《<a href="https://template-party.com/" target="_blank">Web Design:Template-Party</a>》</span>
+    </footer>
+
+  </div>
+  <!--/#container-->
+
+  <!--ページの上部に戻る「↑」ボタン-->
+  <p class="nav-fix-pos-pagetop"><a href="#"><img src="topdist/images/pagetop.png" alt="PAGE TOP"></a></p>
+
+</body>
+
+</html>
